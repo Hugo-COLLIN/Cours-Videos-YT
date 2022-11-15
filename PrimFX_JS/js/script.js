8ec1089183b36b6a71d57e3c7a491c44f54433d5
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-15 22:15:29 +0100

    JS Objets

---
+++ PrimFX_JS/js/script.js
@@ -0,0 +1,55 @@
+
+
+
+// TABLEAUX #2
+
+//let tab = ["elem1", "elem2", "elem3"];
+
+//alert(tab[0]);
+//console.log(tab.indexOf("elem2"));
+/*
+let promptText = prompt("Entrer le nom d\'un element");
+
+if (tab.indexOf(promptText) !== -1)
+    console.log(tab.length)
+
+if (tab.length !== -1) console.log(tab.join(" "));
+
+ */
+
+// OBJETS
+/*
+function Personne (nom, age, sexe)
+{
+    this.nom = nom;
+    this.age = age;
+    this.sexe = sexe;
+}
+
+function Voiture (fab, an, prop)
+{
+    this.fabricant = fab;
+    this.annee = an;
+    this.proprietaire = prop;
+
+    this.genererMessage = function() {
+        alert(this.proprietaire.nom + " possède une " + this.fabricant)
+    }
+}
+
+user1 = new Personne("Jean", 37, "M");
+
+console.log(user1);
+
+v = new Voiture("Soda", "1999", user1);
+
+console.log(v);
+
+v.genererMessage();
+
+v.proprietaire.nom = "Bloc";
+v.genererMessage();
+
+console.log(user1);
+
+ */
